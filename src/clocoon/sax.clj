f9dbe499e5e384ec1f8db8b8153442bb827f7a08
Aaron Donovan <amdonov@gmail.com>
2013-02-26 20:54:08 -0500

    Merged io in sax as it just contained cache code for pipelines at this point.

--- src/clocoon/sax.clj
+++ src/clocoon/sax.clj
@@ -1,15 +1,78 @@
 (ns clocoon.sax
   (:use [clojure.tools.logging :only (info error)]
         [clocoon.filter.core])
-  (:require [clocoon.io :as io]
-            [clocoon.source :as source])
+  (:require [clocoon.source :as source])
   (:import (clocoon.source Source)
            (javax.xml.transform TransformerFactory URIResolver)
            (javax.xml.transform.sax SAXSource SAXTransformerFactory)
            (org.xml.sax InputSource)
            (java.io File ByteArrayInputStream BufferedOutputStream
                     FileOutputStream ByteArrayOutputStream)
-           (java.net URI)))
+           (java.net URI)
+           (java.nio.file Files Paths StandardOpenOption)
+           (java.nio.charset Charset)
+           (java.util Calendar UUID)))
+
+(def ^{:dynamic true} *cachedir* "cache")
+
+(defn get-path 
+  [path & paths]
+  (Paths/get path (into-array String paths)))
+
+(defn make-cache-file []
+  (let [cal (Calendar/getInstance)]
+    (let [p 
+          (get-path *cachedir*
+                    (str (.get cal Calendar/YEAR))
+                    (format "%02d" (+ 1 (.get cal Calendar/MONTH)))
+                    (str (.get cal Calendar/DATE))
+                    (str (.get cal Calendar/DATE))
+                    (format "%02d" (.get cal Calendar/HOUR_OF_DAY))
+                    (format "%02d" (.get cal Calendar/MINUTE))
+                    (str (UUID/randomUUID)))]
+      (.mkdirs (.toFile (.getParent p)))
+      (.toFile p))))
+
+(defn- rewrite-cache
+  "An extra entry is added to the cache journal each time an entry is updated.
+  This leads to multiple lines for each cacheId. This function rebuilds the
+  cache journal to remove old entries."
+  [cache]
+  (info "Opening journal for rebuild")
+  (with-open [file (Files/newBufferedWriter 
+                     (get-path *cachedir* "journal")
+                     (Charset/defaultCharset)
+                     (into-array StandardOpenOption
+                                 (list StandardOpenOption/WRITE
+                                       StandardOpenOption/TRUNCATE_EXISTING
+                                       StandardOpenOption/CREATE)))]
+    (doall 
+      (map (fn [field] 
+             (.write file (str field "||" (get cache field) "\n"))) (keys cache)))))
+
+(defn- build-cache []
+  (let [file (.toFile (get-path *cachedir* "journal"))]
+    (if (.exists file)
+      (let [lines (seq (.split (slurp file) "\n"))
+            fields (map (fn [line] (get (.split line "\\|\\|") 0)) lines)
+            values (map (fn [line] (File. 
+                                     (get (.split line "\\|\\|") 1))) lines)]
+        (let [cache (zipmap fields values)]
+          (rewrite-cache cache)
+          cache))
+      {})))
+
+(def cache (build-cache))
+
+(def journal (do 
+               (info "Opening journal for logging")
+               (.mkdirs (File. *cachedir*))
+               (Files/newBufferedWriter
+                 (get-path *cachedir* "journal")
+                 (Charset/defaultCharset)
+                 (into-array StandardOpenOption
+                             (list StandardOpenOption/APPEND 
+                                   StandardOpenOption/CREATE)))))
 
 (defn- wrap-reader 
   "Wrap an XMLReader with an XMLFilter"
@@ -28,13 +91,13 @@
 
 (defn- do-pipeline [resource serializer filters]
   (let [parser (get-parser resource)
-        file (io/make-cache-file)
+        file (make-cache-file)
         serializer (:constructor serializer)]
     (with-open [os (BufferedOutputStream. (FileOutputStream. file))]
       (apply parser (serializer os) filters))
     file))
 
-(def ^{:private true} pipeline-cache (atom io/cache))
+(def ^{:private true} pipeline-cache (atom cache))
 
 (defn- cached-filter-valid? [ctime f]
   (if (satisfies? CachedFilter f)
@@ -62,8 +125,8 @@
           (if (not (nil? f))
             (.delete f))
           (let [file (do-pipeline resource serializer filters)]
-            (.write io/journal (str cacheId "||" file "\n"))
-            (.flush io/journal)
+            (.write journal (str cacheId "||" file "\n"))
+            (.flush journal)
             (assoc cache cacheId file)))
         cache))))
 
