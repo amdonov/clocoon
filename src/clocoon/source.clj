39cad313d244325dfda0fd2dc9be105653adf5f8
Aaron Donovan <amdonov@gmail.com>
2013-02-26 19:47:55 -0500

    Added back the infoset cache for sources using the new Resource protocol. I'll probably not make it the default behavior going forward.

--- src/clocoon/source.clj
+++ src/clocoon/source.clj
@@ -1,7 +1,8 @@
 (ns clocoon.source
   (:refer-clojure :exclude [resolve])
   (:use [clojure.tools.logging :only (info)])
-  (:require [clocoon.reader :as reader])
+  (:require [clocoon.reader :as reader]
+            [clocoon.serialize :as serialize])
   (:import (java.io ByteArrayInputStream ByteArrayOutputStream File)
            (java.net URL)
            (java.nio.file Files)
@@ -17,18 +18,53 @@
   (modified? [this mtime])
   (fetch [this] [this mtime]))
 
+(defn- with-infoset-cache 
+  "Cache source in memory as byte arrays of fastinfoset content"
+  [cache {:keys [systemId resource]}]
+  (let [r (cache systemId)
+        ctime (if (nil? r) nil (:ctime r))
+        source (if (nil? ctime)
+                 (fetch resource)
+                 (fetch resource ctime))]
+    (if (nil? source)
+      cache
+      (let [{:keys [reader inputSource mtime]} source
+            bos (ByteArrayOutputStream.)]
+        (.setContentHandler reader ((:constructor serialize/infoset) bos))
+        (.parse reader inputSource)
+        (info "Caching infoset copy of" systemId)
+        (assoc cache systemId (CachedSource. (.toByteArray bos) mtime))))))
+
+(def ^{:private true} source-cache (atom {}))
+
 (defrecord CachedResource
-  [resource]
+  [systemId resource]
   Resource
-  (modified? [this mtime]
-    (println "Using Cache")
-    (modified? (:resource this) mtime))
+  (modified? 
+    [this mtime]
+    ; Check the underlining resource to see if
+    ; it's modified. If it is then drop it from the cache
+    (let [res (modified? (:resource this) mtime)]
+      (if res
+        (do
+          (info "Flushing expired content from cache for" (:systemId this))
+          (swap! source-cache (fn
+                                [cache systemId]
+                                (dissoc cache systemId)) (:systemId this))
+          ))
+      res))
   (fetch [this]
-    (println "Using Cache")
-    (fetch (:resource this)))
+    (fetch this nil))
   (fetch [this mtime]
-    (println "Using Cache")
-    (fetch (:resource this) mtime)))
+    (swap! source-cache with-infoset-cache this)
+    (let [systemId (:systemId this)
+          r (@source-cache systemId)
+          ctime (:ctime r)]
+      (if (and (not (nil? mtime)) (<= ctime mtime))
+        nil
+        (let [is (InputSource. (ByteArrayInputStream. (:data r)))]
+          (.setSystemId is systemId)
+          (Source. (reader/get "application/fastinfoset") is ctime))))))
 
 (extend-type File
   Resource
@@ -85,7 +121,7 @@
 
 (defn- cache-resolve
   [systemId]
-  (CachedResource.(resolve systemId)))
+  (CachedResource. systemId (resolve systemId)))
 
 (extend-type String
   Resource
@@ -96,7 +132,3 @@
      (fetch (cache-resolve this)))
     ([this mtime]
      (fetch (cache-resolve this) mtime))))
-
-
-
-
