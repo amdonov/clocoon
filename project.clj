6a7e5fb7f616da5f7cea64191d26699f6f2a2e50
Aaron Donovan <amdonov@gmail.com>
2013-03-03 21:35:38 -0500

    Added a license line

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
             :description "A framework for mapping URLs to SAX pipelines that is similar to Apache Cocoon but written in Clojure"
             :url "http://github.com/amdonov/clocoon"
             :javac-target "1.7"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [ring/ring-core "1.1.8"]
                            [org.clojure/tools.logging "0.2.6"]
@@ -9,3 +11,4 @@
                            [org.xhtmlrenderer/flying-saucer-pdf "9.0.1"]
                            [com.sun.xml.fastinfoset/FastInfoset "1.2.12"]]
             :aot [clocoon.filter.BaseFilter])
+
